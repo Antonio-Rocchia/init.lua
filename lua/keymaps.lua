de3231b20712a2285593aff907cef0d0272d9b00
Antonio Rocchia <tonino.rocchia@gmail.com>
2023-07-20 09:29:04 +0200

    Add keybinds for attached lsp and visual selection

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -5,8 +5,12 @@ A lot of key mapping are set near the plugin for which they are set for
 --]]
 vim.keymap.set({ 'i', 'n' }, '<A-k>', '<Cmd>m -2<Enter>', { desc = 'Move the current line up by one' })
 vim.keymap.set({ 'i', 'n' }, '<A-j>', '<Cmd>m +1<Enter>', { desc = 'Move the current line down by one' })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = 'Move a selection up by one' })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = 'Move a selection down by one' })
 vim.keymap.set({ 'i', 'n' }, '<A-l>', '<Cmd> > <Enter>', { desc = 'Indent line to the right by shiftwidth' })
 vim.keymap.set({ 'i', 'n' }, '<A-h>', '<Cmd> < <Enter>', { desc = 'Indent line to the left by shiftwidth' })
+vim.keymap.set("v", "<A-h>", "<gv", { desc = 'Indent a selection to the right by shiftwidth' })
+vim.keymap.set("v", "<A-l>", ">gv", { desc = 'Indent a selection to the left by shiftwidth' })
 
 vim.keymap.set({ 'i', 'n' }, '<C-k>', "<Cmd>call append(line('.') - 1, repeat([''], v:count1))<CR>",
   { desc = 'Add x lines before current, default = 1' })
@@ -27,7 +31,7 @@ vim.keymap.set('n', '<leader>cdn', vim.diagnostic.goto_next, { desc = 'Jump to t
 vim.keymap.set('n', '<leader>cdf', vim.diagnostic.open_float, { desc = 'Expands floating diagnostic message' })
 vim.keymap.set('n', '<leader>cdl', vim.diagnostic.setloclist, { desc = 'Open the issues list with diagnostic' })
 
-vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Go back to folderview' })
+vim.keymap.set('n', '<leader>e', vim.cmd.Lexplore, { desc = 'Go back to folderview' })
 vim.keymap.set('n', '<leader>cf', vim.cmd.Format, { desc = 'Format the document using the lsp server' })
 
 local telescope_builtin = require 'telescope.builtin'
